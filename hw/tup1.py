0b9bc5673e6b62c8c566b6d3ed02934e7406eb33
Vitalik Yashchuk <114389609+Vitaliy-Yashchuk@users.noreply.github.com>
2024-12-08 20:22:28 +0200

    add tuples hw

---
+++ hw/tup1.py
@@ -0,0 +1,48 @@
+# fructs = ("banana", "apple", "bananamango", "mango", "strawberry-banana")
+# fr = input("Enter your fruct: ")
+
+# task_1
+# print(fructs.count(fr))
+
+# task_2
+# count = sum(1 for word in fructs if fr in word)
+# print(count)
+
+# task_3
+# counter =0
+# auto_companies = (
+#     "Toyota",
+#     "Ford",
+#     "BMW",
+#     "Tesla",
+#     "Mercedes-Benz",
+#     "Volkswagen",
+#     "Honda",
+#     "Chevrolet",
+#     "Nissan",
+#     "Hyundai"
+# )
+# print(auto_companies)
+
+# company = input("Enter your company: ")
+# replace = input("Enter your word: ")
+
+# new_auto_companies = tuple(
+#     replace if word == company else word for word in auto_companies
+# )
+# print(new_auto_companies)
+
+# task_4
+# numbers = (1, 2, 322, 4, 5, 19, 20)
+# count1=0
+# count2=0
+# count3=0
+# for x in numbers:
+#     if x >=0 and x<10:
+#         count1+=1
+#     elif x >=10 and x<100:
+#         count2+=1
+#     elif x >=100 and x<1000:
+#         count3+=1
+# print(f"count1: {count1}\ncount2: {count2}\ncount3: {count3}")
+
