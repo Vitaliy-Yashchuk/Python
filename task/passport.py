4cc6a5d73b0d83ecd7b6c8beb3cc99ee103e4b0b
Vitalik Yashchuk <114389609+Vitaliy-Yashchuk@users.noreply.github.com>
2024-12-15 23:23:36 +0200

    add task passport and covert

--- task/passport.py
+++ task/passport.py
@@ -51,5 +51,38 @@ f1.removeVisa("Poland")
 print(f1.passport_info())
 
 
-# error with creating Figure with abstract methods
-# figure = Figure("Figure")
+class TemperatureConverter:
+    # Статична змінна для підрахунку кількості конвертацій
+    _conversion_count = 0
+
+    @staticmethod
+    def celsius_to_fahrenheit(celsius):
+        """
+        Конвертує температуру з Цельсія у Фаренгейт.
+        Формула: (°C × 9/5) + 32 = °F
+        """
+        TemperatureConverter._conversion_count += 1
+        return (celsius * 9/5) + 32
+
+    @staticmethod
+    def fahrenheit_to_celsius(fahrenheit):
+        """
+        Конвертує температуру з Фаренгейта у Цельсій.
+        Формула: (°F − 32) × 5/9 = °C
+        """
+        TemperatureConverter._conversion_count += 1
+        return (fahrenheit - 32) * 5/9
+
+    @staticmethod
+    def get_conversion_count():
+        """
+        Повертає кількість виконаних конвертацій.
+        """
+        return TemperatureConverter._conversion_count
+
+# Приклад використання
+if __name__ == "__main__":
+    print("25°C у Фаренгейтах:", TemperatureConverter.celsius_to_fahrenheit(25))  # 77.0
+    print("77°F у Цельсіях:", TemperatureConverter.fahrenheit_to_celsius(77))    # 25.0
+
+    print("Кількість виконаних конвертацій:", TemperatureConverter.get_conversion_count()) 
